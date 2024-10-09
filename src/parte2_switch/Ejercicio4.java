66d9bebdbc14ba0086f6f857991b1fca5fd56338
hugo.dominguez <hugo.dominguez@106-08.info2>
2024-10-09 10:01:58 +0200

    ejercicios

---
+++ src/parte2_switch/Ejercicio4.java
@@ -0,0 +1,44 @@
+package parte2_switch;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		
+		double numero;
+		double numero2;
+		String letra;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Elija dos numeros");
+		System.out.println("Numero 1:");
+		numero = sc.nextDouble();
+		System.out.println("Numero 2:");
+		numero2 = sc.nextDouble();
+		
+		System.out.println("Elija una opcion:");
+		System.out.println("A) Sumar los numeros");
+		System.out.println("B) Restar los numeros");
+		System.out.println("C) Multiplicar los numeros");
+		System.out.println("D) Dividir los numeros");
+		letra = sc.next().toUpperCase();
+		
+		switch (letra) {
+		
+		case "A" ->
+			System.out.println("Resultado: "+ (numero+numero2));
+		case "B" ->
+			System.out.println("Resultado: "+ (numero-numero2));
+		case "C" ->
+			System.out.println("Resultado: "+ (numero*numero2));
+		case "D" ->
+			System.out.println("Resultado: "+ (numero/numero2));
+		default ->
+			System.out.println("No existe esa opcion");
+		}
+		sc.close();
+	}
+
+}
